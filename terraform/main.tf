f21165f08bcb836a0f5013f0d4e5a1c7e7e232a4
Michael Scholl <michael.scholl@sysdig.com>
2022-06-16 07:49:41 -0700

    Adding budget across AFT accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,13 @@ resource "aws_s3_account_public_access_block" "block_all_public" {
   ignore_public_acls  = true
   restrict_public_buckets  = true
 }
+
+resource "aws_budgets_budget" "total_cost" {
+  name              = "budget-total-monthly"
+  budget_type       = "COST"
+  limit_amount      = "275"
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2022-06-01_00:00"
+  time_unit         = "MONTHLY"
+}
